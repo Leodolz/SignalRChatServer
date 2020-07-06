fd98d7d3e409d3cfe3254b939157e8c1ac6978e9
Leandro Hurtado <leodolz14@gmail.com>
2020-07-06 18:23:53 -0400

    Added more features

--- ChatServerSignalR/Hubs/ChatHub.cs
+++ ChatServerSignalR/Hubs/ChatHub.cs
@@ -22,19 +22,40 @@ namespace ChatServerSignalR.Hubs
         public async Task SendMessage(string user, string message, string receiver)
         {
             if (!currentChatUsers.ContainsKey(receiver))
-                System.Diagnostics.Debug.WriteLine("User not found");
+                return;
             await Clients.Client(currentChatUsers[receiver].connectionId).SendAsync("ReceiveMessage", user, message);
         }
         public async Task DisconnectFromServer(string user)
         {
             currentChatUsers.Remove(user);
         }
-        
+        public async Task LeaveGame(string user)
+        {
+            await Clients.Client(currentChatUsers[user].connectionId).SendAsync("ReceiveDisconnect");
+        }
+
         public async Task NewConnectionAdded(string user)
         {
             await Clients.AllExcept(Context.ConnectionId).SendAsync("ReceiveMessage", "Server", user + " has joined the chat!");
             if(currentChatUsers.ContainsKey(user))  currentChatUsers.Remove(user);
             currentChatUsers.Add(user, new ConnectedUser { connectionId = Context.ConnectionId, userState = UserStatesConst.USER_AVAILABLE });
+
+        }
+        public async Task<int> ConsultSingleContactState(string userName)
+        {
+            if (!currentChatUsers.ContainsKey(userName)) return Constants.UserStatesConst.USER_ABSENT;
+            return currentChatUsers[userName].userState;
+        }
+        public async Task<int[]> ConsultContactsStates(string[] contacts)
+        {
+            List<int> allContactsState = new List<int>();
+            foreach(string contact in contacts)
+            {
+                if (!currentChatUsers.ContainsKey(contact)) allContactsState.Add(Constants.UserStatesConst.USER_ABSENT);
+                else allContactsState.Add(currentChatUsers[contact].userState);
+            }
+            return allContactsState.ToArray();
+            
         }
         public async Task ShowMessageHistory(string user)
         {
@@ -50,15 +71,22 @@ namespace ChatServerSignalR.Hubs
         {
             await Clients.Client(currentChatUsers[challengedUser].connectionId).SendAsync("ReceiveGameRequest", currentUser, nRounds);
         }
-
+        public async Task SendConnectedToFriends(string[] contacts, string currentUser)
+        {
+            foreach(string contact in contacts)
+            {
+                if (currentChatUsers.ContainsKey(contact))
+                    await Clients.Client(currentChatUsers[contact].connectionId).SendAsync("ReceiveFriendConnect",currentUser);
+            }
+        }
        
 
         public async Task AcceptGameRequest(int nRounds, string challengedUser, string currentUser)
         {
             currentChatUsers[currentUser].userState = UserStatesConst.USER_PLAYING;
             currentChatUsers[challengedUser].userState = UserStatesConst.USER_PLAYING;
-            await Clients.Client(currentChatUsers[currentUser].connectionId).SendAsync("ReceiveGameCode", GameConstants.USER_ACCEPTED, nRounds);
-            await Clients.Client(currentChatUsers[challengedUser].connectionId).SendAsync("ReceiveGameCode", GameConstants.USER_ACCEPTED, nRounds);
+            await Clients.Client(currentChatUsers[currentUser].connectionId).SendAsync("ReceiveGameCode", GameConstants.USER_ACCEPTED, nRounds, challengedUser);
+            await Clients.Client(currentChatUsers[challengedUser].connectionId).SendAsync("ReceiveGameCode", GameConstants.USER_ACCEPTED, nRounds, currentUser);
 
         }
         public async Task SendUsersPick(int sendersPick, string nextUsername)
